7d786e7c38ad3279071e6ec56e6bd2e5d6830346
choi.byoungyun <choi.byoungyun@gmail.com>
2025-04-04 16:06:30 +0900

    - add coc-settings

--- setup.sh
+++ setup.sh
@@ -23,7 +23,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # 4. NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+$curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash$nvm install --lts
+
 
 # 5. Virtualenv
 $sudo apt install python3-pip
